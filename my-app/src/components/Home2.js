0fdd112a275972e605694336a2e157478e0f91d9
Cole-Randolph <84653841+Cole-Randolph@users.noreply.github.com>
2022-03-07 12:00:59 +0530

    1st commit

--- my-app/src/components/Home2.js
+++ my-app/src/components/Home2.js
@@ -1,10 +1,14 @@
 import React from "react";
+import Io from "./io"
 
-const Home2=()=>{
-    return(
+const Home2 = () => {
+    return ( 
         <div>
-            <h1>hello</h1>
+        <h1> hello </h1> 
+        <Io />
         </div>
     )
 }
+
+
 export default Home2;
